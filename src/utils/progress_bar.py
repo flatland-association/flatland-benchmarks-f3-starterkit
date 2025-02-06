8277aab8149ad7e6b7405b6dde5b9eb587db710d
Adrian Egli <u216993@sbb.ch>
2025-02-06 22:38:42 +0100

    make debug output more pretty :-)

--- src/utils/progress_bar.py
+++ src/utils/progress_bar.py
@@ -3,10 +3,11 @@ import numpy as np
 
 class ProgressBar:
     @staticmethod
-    def console_print(itr: float, max_value: float, info: str = ''):
+    def console_print(itr: float, max_value: float, info: str = '', start_str='\r'):
         percent = max(0.0, min(100.0, (itr / max_value * 100)))
         i_percent = 100.0 - percent
-        print('\r{}{} {:5.1f}% {}'.format("#" * int(np.ceil(percent)),
+        print('{}{}{} {:5.1f}% {}'.format(start_str,
+                                          "#" * int(np.ceil(percent)),
                                           "-" * int(np.ceil(i_percent)),
                                           percent,
                                           info), end=' ')
